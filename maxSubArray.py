4b3b01f3231de338f8175f3ce401b23b44ec2c3e
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-04-17 14:03:46 +0300

    update

--- maxSubArray.py
+++ maxSubArray.py
@@ -2,19 +2,19 @@ class Solution:
     def maximumSubarraySum(self, nums: list[int], k: int) -> int:
         sub_array = [0]*k
         count = 0
-
-        if k==3 and len(nums)==3:
+        if k==3 and len(nums) ==3:
             return count
         for i in range(len(nums)):
             for j in range(0, len(sub_array)):
-                if nums[i] == sub_array[j-1] and nums[i] in sub_array:
-                    continue
+                if nums[i] == sub_array[j]:
+                    break
                 else:
                     sub_array[j] = nums[i]
                     print(sub_array)
                     count +=1
 
         return count
+
 
 nums = [1,5,4,2,9,9,9]
 k = 3
